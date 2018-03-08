34aff5d3910d536515d9e085fbde7dec5edf94ba
vimleshtech <35063663+vimleshtech@users.noreply.github.com>
2018-03-08 00:44:16 -0600

    Prog1

---
+++ prog2.ps1
@@ -0,0 +1,15 @@
+﻿#loop : is iterator or repeation of statement/command/code/logic 
+#example : 1 2 ...1000
+#fundamental of loop:   i. intialziation  ii. condition iii. increment/decrement
+# i. init/start       = 1              1000
+# ii. condition/limit = 1000           1 
+# iii. increment/steps = +1            -1
+
+#there are following types of loop:
+#i. for loop 
+
+for($i=1; $i -le 10;  $i++)
+{
+    $i 
+}
+
